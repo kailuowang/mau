3ee22b5da55e4967b01b6ebd67c0fd49206a6c35
Kailuo Wang <kailuo.wang@gmail.com>
2019-08-29 20:31:50 -0400

    format code

--- core/src/main/scala/mau/RefeshRef.scala
+++ core/src/main/scala/mau/RefeshRef.scala
@@ -9,30 +9,28 @@ import mau.RefreshRef.Item
 
 import scala.concurrent.duration.FiniteDuration
 
-class RefreshRef[F[_], V] private(ref: Ref[F, Option[Item[F, V]]], onRefreshed: V => F[Unit])(
-  implicit F: Concurrent[F], T: Timer[F]) {
+class RefreshRef[F[_], V] private (
+    ref: Ref[F, Option[Item[F, V]]],
+    onRefreshed: V => F[Unit])(implicit F: Concurrent[F], T: Timer[F]) {
 
-  def cancel: F[Boolean] = ref.modify {
-    case None => (None, F.pure(false))
-    case Some(Item(_, f, _)) => (None, f.cancel *> F.pure(true))
-  }.flatten
-
+  def cancel: F[Boolean] =
+    ref.modify {
+      case None                => (None, F.pure(false))
+      case Some(Item(_, f, _)) => (None, f.cancel *> F.pure(true))
+    }.flatten
 
   def getOrFetch(period: FiniteDuration)(fetch: F[V]): F[V] =
     get(period, None)(fetch)(PartialFunction.empty)
 
-  def getOrFetch(period: FiniteDuration, staleTimeout: FiniteDuration)
-                (fetch: F[V])
-                (errorHandler: PartialFunction[Throwable, F[Unit]])
-  : F[V] = get(period, Some(staleTimeout))(fetch)(errorHandler)
+  def getOrFetch(period: FiniteDuration, staleTimeout: FiniteDuration)(
+      fetch: F[V])(errorHandler: PartialFunction[Throwable, F[Unit]]): F[V] =
+    get(period, Some(staleTimeout))(fetch)(errorHandler)
 
   def get: F[Option[V]] = ref.get.map(_.map(_.v))
 
-
-  private def get(period: FiniteDuration, staleTimeoutO: Option[FiniteDuration])
-                 (fetch: F[V])
-                 (errorHandler: PartialFunction[Throwable, F[Unit]])
-  : F[V] = {
+  private def get(period: FiniteDuration,
+                  staleTimeoutO: Option[FiniteDuration])(fetch: F[V])(
+      errorHandler: PartialFunction[Throwable, F[Unit]]): F[V] = {
     def startRefresh: F[V] = {
 
       def onFetchError(e: Throwable): F[Unit] = {
@@ -42,7 +40,8 @@ class RefreshRef[F[_], V] private(ref: Ref[F, Option[Item[F, V]]], onRefreshed: 
               itemO <- ref.get
               now <- nowF
             } yield
-              itemO.fold(false)(_.lastFetch.isBefore(now.minusNanos(timeout.toNanos)))
+              itemO.fold(false)(
+                _.lastFetch.isBefore(now.minusNanos(timeout.toNanos)))
           }
 
         errorHandler.lift(e) match {
@@ -50,7 +49,7 @@ class RefreshRef[F[_], V] private(ref: Ref[F, Option[Item[F, V]]], onRefreshed: 
             cancel.void
           case Some(fu) =>
             (fu >> isStale).flatMap { stale =>
-              if(stale) cancel.void
+              if (stale) cancel.void
               else loop
             }
         }
@@ -68,26 +67,25 @@ class RefreshRef[F[_], V] private(ref: Ref[F, Option[Item[F, V]]], onRefreshed: 
                 _ <- ref.tryUpdate(_.map(_.copy(v = v, lastFetch = now)))
                 _ <- loop
               } yield ()
-
           }
-
 
       for {
         initialV <- fetch
         fiber <- F.start(loop)
         now <- nowF
-        registeredO  <- ref.tryModify {
-          case None => (Some(Item(initialV, fiber, now)), true)
+        registeredO <- ref.tryModify {
+          case None        => (Some(Item(initialV, fiber, now)), true)
           case e @ Some(_) => (e, false)
         }
-        _ <- if(registeredO.fold(false)(identity)) F.unit else
+        _ <- if (registeredO.fold(false)(identity)) F.unit
+        else
           fiber.cancel //cancel if setting refresh fails
       } yield initialV
     }
 
     ref.get.flatMap {
       case Some(Item(v, _, _)) => v.pure[F]
-      case None => startRefresh
+      case None                => startRefresh
     }
   }
 
@@ -96,17 +94,20 @@ class RefreshRef[F[_], V] private(ref: Ref[F, Option[Item[F, V]]], onRefreshed: 
 
 object RefreshRef {
 
-  private case class Item[F[_], V](v: V, refresh: Fiber[F, Unit], lastFetch: Instant)
+  private case class Item[F[_], V](v: V,
+                                   refresh: Fiber[F, Unit],
+                                   lastFetch: Instant)
 
-  def create[F[_]: Concurrent: Timer, V](onRefreshed: V => F[Unit]): F[RefreshRef[F, V]] =
+  def create[F[_]: Concurrent: Timer, V](
+      onRefreshed: V => F[Unit]): F[RefreshRef[F, V]] =
     Ref.of(none[Item[F, V]]).map(new RefreshRef[F, V](_, onRefreshed))
 
   /**
-   * Cancel itself after use
-   */
-  def resource[F[_]: Concurrent: Timer, V](onRefreshed: V => F[Unit]): Resource[F, RefreshRef[F, V]] =
+    * Cancel itself after use
+    */
+  def resource[F[_]: Concurrent: Timer, V](
+      onRefreshed: V => F[Unit]): Resource[F, RefreshRef[F, V]] =
     Resource.make(create[F, V](onRefreshed))(_.cancel.void)
-
 
   def resource[F[_]: Concurrent: Timer, V]: Resource[F, RefreshRef[F, V]] =
     resource[F, V]((_: V) => Concurrent[F].unit)
